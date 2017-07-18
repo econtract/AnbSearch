4dba5f91014f46cc7c72f87c4989cd65a071c1d9
Imran <imran@zeropoint.it>
2017-07-18 17:01:46 +0500

    Translations are now part of the plugin.

--- pll-register-trans.php
+++ pll-register-trans.php
@@ -9,22 +9,25 @@ if ( !function_exists( 'pll_register_string' ) ) {
     require_once WP_PLUGIN_DIR . '/polylang/include/api.php';
 }
 
-pll_register_string('Services', 'Services', 'AnbSearch');
-pll_register_string('Internet', 'Internet', 'AnbSearch');
-pll_register_string('TV', 'TV', 'AnbSearch');
-pll_register_string('Fixed line', 'Fixed line', 'AnbSearch');
-pll_register_string('Mobile', 'Mobile', 'AnbSearch');
-pll_register_string('Installation area', 'Installation area', 'AnbSearch');
-pll_register_string('Enter Zipcode', 'Enter Zipcode', 'AnbSearch');
-pll_register_string('Type of Use', 'Type of Use', 'AnbSearch');
-pll_register_string('Private', 'Private', 'AnbSearch');
-pll_register_string('Business', 'Business', 'AnbSearch');
-pll_register_string('Search Deals', 'Search Deals', 'AnbSearch');
+function anbSearchTrans() {
+    pll_register_string('Services', 'Services', 'AnbSearch');
+    pll_register_string('Internet', 'Internet', 'AnbSearch');
+    pll_register_string('TV', 'TV', 'AnbSearch');
+    pll_register_string('Fixed line', 'Fixed line', 'AnbSearch');
+    pll_register_string('Mobile', 'Mobile', 'AnbSearch');
+    pll_register_string('Installation area', 'Installation area', 'AnbSearch');
+    pll_register_string('Enter Zipcode', 'Enter Zipcode', 'AnbSearch');
+    pll_register_string('Type of Use', 'Type of Use', 'AnbSearch');
+    pll_register_string('Private', 'Private', 'AnbSearch');
+    pll_register_string('Business', 'Business', 'AnbSearch');
+    pll_register_string('Search Deals', 'Search Deals', 'AnbSearch');
+    pll_register_string('Provider preferences', 'Provider preferences', 'AnbSearch');
+    pll_register_string('Select Provider', 'Select Provider', 'AnbSearch');
+    pll_register_string('Need help?', 'Need help?', 'AnbSearch');
+    pll_register_string('We\'ll guide you', 'We\'ll guide you', 'AnbSearch');
+    pll_register_string('Search', 'Search', 'AnbSearch');
+    pll_register_string('Select the service you like to compare', 'Select the service you like to compare', 'AnbSearch');
+    pll_register_string('Need help?', 'Need help?', 'AnbSearch');
+}
 
-pll_register_string('Provider preferences', 'Provider preferences', 'AnbSearch');
-pll_register_string('Select Provider', 'Select Provider', 'AnbSearch');
-pll_register_string('Need help?', 'Need help?', 'AnbSearch');
-pll_register_string('We\'ll guide you', 'We\'ll guide you', 'AnbSearch');
-pll_register_string('Search', 'Search', 'AnbSearch');
-pll_register_string('Select the service you like to compare', 'Select the service you like to compare', 'AnbSearch');
-pll_register_string('Need help?', 'Need help?', 'AnbSearch');
+add_action('init', 'anbSearchTrans');
